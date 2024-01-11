159e6d101d8bd5e8a1651c43f4db59e75b044ddb
Adrianto Hartono <128348064+AdriantoHartono@users.noreply.github.com>
2024-01-11 19:51:33 +0800

    Update single.php

--- Version 2/single.php
+++ Version 2/single.php
@@ -9,10 +9,7 @@ get_header();
 			the_post();
 			get_template_part( 'template-parts/content', get_post_type() );
 			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'openhouse_a2' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'openhouse_a2' ) . '</span> <span class="nav-title">%title</span>',
-				)
+
 			);
 
 			if ( comments_open() || get_comments_number() ) :
